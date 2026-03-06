bb43a24f12c2fb9a64945ccef11c8d70e8069779
Sumeet951 <sg297979@gmail.com>
2026-03-06 14:10:58 +0530

    Changes url

--- frontend-deliverytimePrediction/src/App.jsx
+++ frontend-deliverytimePrediction/src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
     setLoading(true);
     setPrediction(null);
     try {
-      const res = await fetch("http://127.0.0.1:8000/predict", {
+      const res = await fetch("https://job-portal-mu-ochre.vercel.app/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
